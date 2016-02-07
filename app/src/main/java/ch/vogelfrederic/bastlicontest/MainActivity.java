4587c85402970d108b6ed23295e845a7368da486
vogelfr <vogelfr@student.ethz.ch>
2016-02-07 14:08:30 +0100

    Color picker etc..

--- app/src/main/java/ch/vogelfrederic/bastlicontest/MainActivity.java
+++ app/src/main/java/ch/vogelfrederic/bastlicontest/MainActivity.java
@@ -1,10 +1,15 @@
 package ch.vogelfrederic.bastlicontest;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -13,6 +18,8 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     public static DatagramSocket datagramSocket;
     public static final int NUM_STRIPS = 15;
     public static final int NUM_LEDS = 112;
+    private ToggleButton toggle;
+    private Button update;
+    private Button selectColor;
+    private int[] colors = {0,0,0};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,38 +52,69 @@ public class MainActivity extends AppCompatActivity {
             Log.d("UDP", "Socket exists");}
         catch (Exception e) {Log.d("3", "Hello");}
 
+        toggle = (ToggleButton) findViewById(R.id.toggleButton_toggle);
+        update = (Button) findViewById(R.id.button_update);
+        selectColor = (Button) findViewById(R.id.button_selectColor);
+
     }
 
-    public void allOff(View v) {
+    public void toggleLights(View v) {
         for (int i = 0; i < NUM_STRIPS; i++) {
             byte[] data;
             data = new byte[1+NUM_LEDS*3];
             data[0] = (byte) i;
             for (int j = 1; j < data.length; j++) {
-                data[j] = 0;
+                data[j] = toggle.isChecked()? (byte) 255 : 0;
             }
 
-            AsyncTask<byte[], Void, Void> async = new AsyncTask<byte[], Void, Void>() {
-                @Override
-                protected Void doInBackground(byte[]... params) {
-                    byte[] data = params[0];
-                    for (int t = 0; t < 1; t++) {
-                        try {
-                            DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
-                            Log.d("UDP", "Sending data (length: " + data.length + ")");
-                            datagramSocket.send(packet);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    return null;
-                }
-            };
-
-            async.execute(data);
-
+            Task task = new Task(datagramSocket, address, port);
+            task.execute(data);
         }
-
     }
+
+    public void updateColor(View v) {
+        for (int i = 0; i < NUM_STRIPS; i++) {
+            byte[] data;
+            data = new byte[1+NUM_LEDS*3];
+            data[0] = (byte) i;
+            for (int j = 1; j < data.length; j++) {
+                switch (j % 3) {
+                    case 1:
+                        data[j] = (byte) colors[0];
+                        break;
+                    case 2:
+                        data[j] = (byte) colors[1];
+                        break;
+                    default:
+                        data[j] = (byte) colors[2];
+                        break;
+                }
+            }
+
+            Task task = new Task(datagramSocket, address, port);
+            task.execute(data);
+        }
+    }
+
+    public void pickColor(View v) {
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, 0xff0000ff, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                Log.d("COLOR", String.valueOf(color));
+                colors[0] = Color.red(color);
+                colors[1] = Color.green(color);
+                colors[2] = Color.blue(color);
+                selectColor.setBackgroundColor(color);
+            }
+        });
+
+        colorPicker.show();
+    }
+
+
 
 }
